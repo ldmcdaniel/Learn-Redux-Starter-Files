5377e4b4c28ba8cb0134549d839d278f54bbaf7b
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-02-20 14:24:24 -0600

    Added routes for different components in router, functioning routes

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">Single Photo</div>
+        )
+    }
+});
+
+export default Single;
