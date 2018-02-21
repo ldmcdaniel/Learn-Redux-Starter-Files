d922eed962f0b6248853a48c27e93c70caf7cfbd
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-02-21 13:56:40 -0600

    Added Comments to Single component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                This is a comment
+            </div>
+        );
+    }
+});
+
+export default Comments;
