45522edbf3e4b50835755c2275573bee056f541f
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2018-02-22 10:19:41 -0600

    Added config file for sentry

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = 'a70f9fdb4b3a4491bb7c72d433c330d4';
+const sentry_app = '292677';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
